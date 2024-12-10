bff3080c00967799fe22a2ee8035163e76e48d37
Selman Orhan <selmanorhan123@gmail.com>
2024-12-10 17:34:56 +0300

    First Day some experiments on maps images and text

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Selman Orhan on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
