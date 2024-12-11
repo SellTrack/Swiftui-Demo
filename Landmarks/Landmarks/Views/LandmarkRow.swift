d6b9a85c528441ac018c8d57e3285258d983f0f8
Selman Orhan <selmanorhan123@gmail.com>
2024-12-11 11:28:33 +0300

    navigation panels added.

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Selman Orhan on 11.12.2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
