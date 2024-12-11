48074d4071d80d5626f94db38b385f421a7478d2
Selman Orhan <selmanorhan123@gmail.com>
2024-12-11 12:07:00 +0300

    Favorite buton eklenerek filtreleme ozelligi kelendi

---
+++ Landmarks/Landmarks/Views/Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Selman Orhan on 2.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+
+}
