b7e483c0528ae5a811d69226b3baa23ff4055357
Selman Orhan <selmanorhan123@gmail.com>
2024-12-12 09:36:58 +0300

    a main page designed for more swiftui components

---
+++ Landmarks/Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Selman Orhan on 12.12.2024.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
